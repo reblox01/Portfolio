e4b04a92a7fc7de09ff63a96428b7f9a4384a40d
0x8D <mirocairo15@gmail.com>
2024-09-01 17:46:50 +0100

    Add framer motion animation on the components like about, experiences, projects, navbar, techtasks and contact components & fix some issues in performance

--- components/Card.tsx
+++ components/Card.tsx
@@ -25,40 +25,38 @@ const Card = ({ OneLiner, screenshot, title, href, source }: Prop) => {
   };
 
   return (
-    <div className="relative row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border justify-between flex flex-col">
+    <div className="relative flex flex-col justify-between h-full rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border">
       <Link href={href}>
-        <>
-          <div className="rounded-xl border">
+        <div className="flex flex-col h-full">
+          <div className="flex-shrink-0 rounded-xl border overflow-hidden">
             <Image
-              className="rounded-xl"
+              className="object-cover rounded-xl"
               src={screenshot}
               alt={title}
               width={400}
               height={300}
             />
           </div>
-          <div className="group-hover/bento:translate-x-2 transition duration-200">
-            <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
+          <div className="flex-grow mt-2 group-hover/bento:translate-x-2 transition duration-200">
+            <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2">
               {title}
             </div>
             <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
               {OneLiner}
             </div>
           </div>
-        </>
+        </div>
       </Link>
-      <div className="mt-4">
-        {source && (
-          <a
-            href={source}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="absolute bottom-3 right-4 text-neutral-600 dark:text-neutral-200"
-          >
-            {getSourceIcon(source)}
-          </a>
-        )}
-      </div>
+      {source && (
+        <a
+          href={source}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="absolute bottom-3 right-4 text-neutral-600 dark:text-neutral-200"
+        >
+          {getSourceIcon(source)}
+        </a>
+      )}
     </div>
   );
 };
