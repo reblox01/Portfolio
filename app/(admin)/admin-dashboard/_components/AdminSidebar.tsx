222b240c37989cfa42f2be74570920331e33e1f0
0x8D <mirocairo15@gmail.com>
2025-08-05 12:10:28 +0100

    Fix href for Certifications route in AdminSidebar component

--- app/(admin)/admin-dashboard/_components/AdminSidebar.tsx
+++ app/(admin)/admin-dashboard/_components/AdminSidebar.tsx
@@ -41,7 +41,7 @@ const routes: Route[] = [
   },
   {
     label: "Certifications",
-    href: "/admin-dashboard/manage-certifications",
+    href: "/admin-dashboard/manage-certification",
     icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 15a7 7 0 1 0 0-14 7 7 0 0 0 0 14Z" /><path d="M8.21 13.89 7 23l5-3 5 3-1.21-9.12" /></svg>
   },
   {
